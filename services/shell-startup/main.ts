a54cf3dd3ab2f83d203f513826f4cd42b758c154
IlyaKotomin <mostlys4nny@gmail.com>
2024-04-15 01:06:57 +0300

    Added cursor init in main.ts

--- services/shell-startup/main.ts
+++ services/shell-startup/main.ts
@@ -2,6 +2,8 @@ import "resources/Global"
 import { BarView } from "../../mvvm/views/bar/bar-view";
 
 App.config({
+    gtkTheme: "Adwaita-dark",
+    cursorTheme: "GoogleDot-Black",
     windows: [
         BarView.BuildBar(0),
         BarView.BuildBar(1),
