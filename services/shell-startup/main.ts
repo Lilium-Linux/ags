08f047462a7c4e5629961eeaa17b54f6066dcb6a
IlyaKotomin <mostlys4nny@gmail.com>
2024-04-15 01:15:01 +0300

    Added cursor init in main.ts 2

--- services/shell-startup/main.ts
+++ services/shell-startup/main.ts
@@ -1,9 +1,10 @@
 import "resources/Global"
 import { BarView } from "../../mvvm/views/bar/bar-view";
+import GLib from "gi://GLib?version=2.0"
 
 App.config({
     gtkTheme: "Adwaita-dark",
-    cursorTheme: "GoogleDot-Black",
+    cursorTheme: GLib.getenv("XCURSOR_THEME")!,
     windows: [
         BarView.BuildBar(0),
         BarView.BuildBar(1),
