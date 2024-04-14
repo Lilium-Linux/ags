3cb0740d2eda0fefad72dfa8cbfffb8930b8f4f3
IlyaKotomin <mostlys4nny@gmail.com>
2024-04-15 02:50:01 +0300

    changed compiled style name

--- services/style/sass-controller.ts
+++ services/style/sass-controller.ts
@@ -1,7 +1,7 @@
 export class SassController {
 
     private static scss = `${App.configDir}/services/style/main.scss`;
-    private static css = `${App.configDir}/services/style/_compiled-main.css`;
+    private static css = `${App.configDir}/services/style/compiled-main.css`;
     public static LoadCss(){
         this.applyScss();
         Utils.monitorFile(this.css, this.applyScss);
