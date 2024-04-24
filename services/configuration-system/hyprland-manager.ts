669ae6007fdca313d27977ff624cdf8a2ca0d0b0
IlyaKotomin <mostlys4nny@gmail.com>
2024-04-25 00:09:01 +0300

    Fixed hyprland-manager.ts

--- services/configuration-system/hyprland-manager.ts
+++ services/configuration-system/hyprland-manager.ts
@@ -19,7 +19,7 @@ export class HyprlandManager
 
         await this.setHyprlandConfig("general:gaps_in", gapsIn.toString());
         await this.setHyprlandConfig("general:gaps_out", gapsOut.toString());
-        await this.setHyprlandConfig("decorations:rounding", rounding.toString());
+        await this.setHyprlandConfig("decoration:rounding", rounding.toString());
         await this.setHyprlandConfig("general:col.active_border", activeBorder);
         //await this.setHyprlandConfig("general:col.inactive_border", LayoutManager.layout.gaps.toString());
     }
