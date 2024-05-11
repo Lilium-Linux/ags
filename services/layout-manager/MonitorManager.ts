c31f3299a93dd58165695d233cf6d87c8f148521
IlyaKotomin <mostlys4nny@gmail.com>
2024-05-12 00:58:05 +0300

    Changed logger in MonitorManager.ts

--- services/layout-manager/MonitorManager.ts
+++ services/layout-manager/MonitorManager.ts
@@ -1,9 +1,8 @@
 import Gdk from "gi://Gdk";
 import Gtk from "@girs/gtk-3.0";
 
-export function range(length: number, start = 1) {
-    console.log("range() called");
-    return Array.from({ length }, (_, i) => i + start)
+function range(start: number, end: number): number[] {
+    return Array.from({ length: end - start + 1 }, (_, index) => index + start);
 }
 
 export class MonitorManager {
@@ -11,7 +10,7 @@ export class MonitorManager {
         console.log("Looking for monitors...");
         const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
         console.log(`Monitors count: ${n}`);
-        return range(n, 0).flatMap(widget);
+        return range(0, n).flatMap(widget);
     }
 
     // private static range(length: number, start = 1) {
