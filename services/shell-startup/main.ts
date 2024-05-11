ffb0af5633eed619e4b0b176ab18754bc8f6b473
IlyaKotomin <mostlys4nny@gmail.com>
2024-05-12 02:44:20 +0300

    Changed _bar.scss

--- services/shell-startup/main.ts
+++ services/shell-startup/main.ts
@@ -2,9 +2,7 @@ import "resources/Global"
 import { BarView } from "../../mvvm/views/bar/bar-view";
 import GLib from "gi://GLib?version=2.0"
 import * as styleController from "services/style/sass-controller"
-import { PaletteManager } from "../configuration-system/palette-manager";
 import { ConfigLoader } from "../configuration-system/config-loader";
-import { MonitorManager } from "../layout-manager/MonitorManager";
 import { GlobalWidget } from "../../Contracts/Widgets/GlobalWidget";
 
 await ConfigLoader.load();
