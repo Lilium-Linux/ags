64d40961e1d5548348ae6419674742c2f05f760a
IlyaKotomin <mostlys4nny@gmail.com>
2024-05-12 00:58:29 +0300

    Changed logger in MonitorManager.ts

--- services/layout-manager/MonitorManager.ts
+++ services/layout-manager/MonitorManager.ts
@@ -10,7 +10,7 @@ export class MonitorManager {
         console.log("Looking for monitors...");
         const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
         console.log(`Monitors count: ${n}`);
-        return range(0, n).flatMap(widget);
+        return range(1, n).flatMap(widget);
     }
 
     // private static range(length: number, start = 1) {
