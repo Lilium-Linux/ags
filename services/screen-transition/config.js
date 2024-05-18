8d593d4dbe6dbe3524d17f940fa64fe2fae0ba1b
kotoxik <mostlys4nny@gmail.com>
2024-05-19 02:42:28 +0300

    Changed screen transition

--- services/screen-transition/config.js
+++ services/screen-transition/config.js
@@ -1,12 +1,22 @@
+import Gdk from "@girs/gdk-3.0";
+
 const screen = Widget.Window({
     name: 'transition',
-    anchor: ['top', 'left', 'right', ],
-    child: Widget.DrawingArea(
-
-    ),
+    layer: "overlay",
+    anchor: ['top', 'left', 'right', 'bottom'],
+    child: Widget.Calendar()
+    // child: Widget.DrawingArea(
+    //
+    // ),
 })
+
+const range = (length, start = 1) => Array.from({ length }, (_, i) => i + start);
+function forMonitors(widget) {
+    const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
+    return range(n, 0).map(widget).flat(1);
+}
 
 App.config({
     style: `${App.configDir}/transition.css`,
-    windows: [screen]
+    windows: [...forMonitors(screen)]
 })
