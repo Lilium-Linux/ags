0b7978b08186408dfeee77a89587e80f530cb08b
kotoxik <mostlys4nny@gmail.com>
2024-05-18 20:46:18 +0300

    Changed workspaces buttons style

--- mvvm/view-models/bar/bar-view-model.ts
+++ mvvm/view-models/bar/bar-view-model.ts
@@ -63,7 +63,7 @@ export class BarViewModel{
             return items.map(item => {
                 // Define button properties
                 const buttonProps = {
-                    child: Widget.Icon({ icon: item.bind("icon"), size: 40 }),
+                    child: Widget.Icon({ icon: item.bind("icon"), size: 30 }),
                     on_primary_click: (_, event) => item.activate(event),
                     on_secondary_click: (_, event) => item.openMenu(event),
                     tooltip_markup: item.bind("tooltip_markup"),
