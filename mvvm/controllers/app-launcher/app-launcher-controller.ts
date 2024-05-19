037e7212bd3cab230a6df8f3baee7ebcbcd1a5ed
kotoxik <mostlys4nny@gmail.com>
2024-05-19 23:40:24 +0300

    App launcher demo 2

--- mvvm/controllers/app-launcher/app-launcher-controller.ts
+++ mvvm/controllers/app-launcher/app-launcher-controller.ts
@@ -2,27 +2,15 @@ import { AppLauncherView } from "../../views/app-launcher/app-launcher-view";
 
 
 export class AppLauncherController{
-
-    public static async tryToggle() {
-        // Wrap the synchronous call in a promise
-        const window = await new Promise<any>((resolve) => {
-            const win = App.getWindow(AppLauncherView.getName());
-            resolve(win);
-        });
-
-        if (window == null) {
-            await new Promise<void>((resolve) => {
-                App.addWindow(new AppLauncherView().getWindow());
-                resolve();
-            });
+    public static tryToggle(){
+        let window= App.getWindow(AppLauncherView.getName());
+        if (window == undefined){
+            window = new AppLauncherView().getWindow();
+            App.addWindow(window);
         }
 
-        await new Promise<void>((resolve) => {
-            App.toggleWindow(AppLauncherView.getName());
-            resolve();
-        });
-
+        window.set_visible(!window.visible)
+        //App.toggleWindow(AppLauncherView.getName());
         console.log(`Launcher toggled: ${AppLauncherView.getName()}`);
     }
-
 }
